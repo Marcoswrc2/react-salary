67ca3103cdcd858f82acbdf0037738fb92a6aa27
Fermento <fermento@MacBook-Pro-de-Fermento-2.local>
2020-06-17 13:08:18 -0300

    Updates

---
+++ precache-manifest.88c7e66259d1d0300a31ba39efcaceee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20837686c9bffc886682e08e54c6d176",
+    "url": "/react-salary/index.html"
+  },
+  {
+    "revision": "9cb9f1818834dfb59e63",
+    "url": "/react-salary/static/css/main.8a357863.chunk.css"
+  },
+  {
+    "revision": "bb1882150c8b63abb55f",
+    "url": "/react-salary/static/js/2.e1b35861.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-salary/static/js/2.e1b35861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cb9f1818834dfb59e63",
+    "url": "/react-salary/static/js/main.6a1e783c.chunk.js"
+  },
+  {
+    "revision": "b8478ce8ee65e9c0b40c",
+    "url": "/react-salary/static/js/runtime-main.6bd9649a.js"
+  }
+]);
